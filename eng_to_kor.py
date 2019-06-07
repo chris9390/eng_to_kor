695af6fd58df122f5f32fcdbc1bfcb78659bc641
Kim Young-in <chris9390@naver.com>
2019-06-07 17:22:03 +0900

    - 읽는 방식이 정의된 영어의 경우 딕셔너리를 구성해서 변경
    - 그 나머지는 알파벳 하나하나 한글 읽기로 변경

--- eng_to_kor.py
+++ eng_to_kor.py
@@ -12,12 +12,19 @@ from bs4 import BeautifulSoup
 from my_news_normalizer import my_news_normalizer
 import re
 
+
 eng_kor_dict = {'a' : '에이', 'b' : '비', 'c' : '씨', 'd' : '디', 'e' : '이',
                 'f' : '에프','g' : '쥐' ,'h' : '에이치' ,'i' : '아이' ,'j' : '제이',
                 'k' : '케이', 'l' : '엘', 'm' : '엠', 'n' : '엔', 'o' : '오',
                 'p' : '피', 'q' : '큐', 'r' : '알', 's' : '에스', 't' : '티',
                 'u' : '유',  'v' : '브이', 'w' : '더블유', 'x' : '엑스', 'y' : '와이', 'z' : '지'}
 
+
+read_like_this = {'kist' : '키스트', 'sars' : '사스', 'kaist' : '카이스트', 'kal' : '칼', 'naver' : '네이버', 'posco' : '포스코',
+                  'koica' : '코이카', 'lifeplus' : '라이프플러스', 'korea' : '코리아', 'etri' : '에트리', 'opec' : '오펙'}
+
+# R&D, M&A
+eng_n_eng_pattern = re.compile(r'([a-zA-Z]+\s*\&\s*[a-zA-Z]+)')
 
 # 영어 패턴
 eng_pattern = re.compile(r'([a-zA-Z]+)')
@@ -35,14 +42,16 @@ conn = pymysql.connect(host='163.239.28.23',
 
 c = conn.cursor()
 
-sql = "SELECT * from core_article LIMIT 10, 20"
+sql = "SELECT * from core_article LIMIT 0, 1000"
 
 c.execute(sql)
 rows = c.fetchall()
 
+
+
 for row in rows:
-    print('\n{} - {} - {}'.format(row['article_uploaded_at'], row['article_title'], row['article_url']))
-    print('=========================================================================================================')
+    #print('\n{} - {} - {}'.format(row['article_uploaded_at'], row['article_title'], row['article_url']))
+    #print('=========================================================================================================')
 
 
     article_raw = row['article_raw']
@@ -51,14 +60,55 @@ for row in rows:
     content_text = html_obj.text
     divided_text = my_news_normalizer(content_text)
 
-    for sentence in divided_text:
-        #print(sentence)
+    # 기사 내에 영어가 있는 경우만 출력
+    if eng_pattern.findall(' '.join(divided_text)):
+        print('\n{} - {} - {}'.format(row['article_uploaded_at'], row['article_title'], row['article_url']))
+        print('=========================================================================================================')
 
-        eng = eng_pattern.findall(sentence)
-        if eng:
-            print(sentence)
+        for sentence in divided_text:
 
-    print('=========================================================================================================\n')
+            # 알파벳 소문자로 변환
+            sentence = sentence.lower()
+
+            # 각 문장에 영어가 있는 경우
+            eng = eng_pattern.findall(sentence)
+            if eng:
+                # 변경 전
+                print(sentence)
+
+                # 읽는 방법이 있는 영어인 경우 미리 저장해놓은 딕셔너리를 통해 변경
+                for each in read_like_this:
+                    if each in sentence:
+                        sentence = sentence.replace(each, '<<' + read_like_this[each] + '>>')
+
+
+                # R&D -> R앤D
+                eng_n_eng = eng_n_eng_pattern.findall(sentence)
+                if eng_n_eng:
+                    for each in eng_n_eng:
+                        temp = each.replace('&', '앤')
+                        sentence = sentence.replace(each, temp)
+
+
+                # 나머지 모든 알파벳 각각 읽기 방식으로 변경
+                #  a -> 에이 / b -> 비
+                for each in eng_kor_dict:
+                    if each in sentence:
+                        sentence = sentence.replace(each, eng_kor_dict[each])
+
+
+                # 영어 표기 색칠
+                '''
+                for each in eng:
+                    sentence = sentence.replace(each, '\033[1m\033[31m' + each + '\033[0m')
+                '''
+
+                # 변경 후
+                print(sentence )
+                print('\n')
+
+
+        print('=========================================================================================================\n')
 
 
 c.close()
